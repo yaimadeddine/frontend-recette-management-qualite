041b2fd9bbae274c3612979b0809b797908d9e4b
Ouassen123 <104273085+Ouassen123@users.noreply.github.com>
2023-12-20 23:44:34 +0100

    comit15

--- front-end-recette-management-qualite-angular/src/app/app-routing.module.ts
+++ front-end-recette-management-qualite-angular/src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AuthentificationComponent } from './components/authentification/authentification.component';
 import { GestionRecettesComponent } from './components/gestion-recettes/gestion-recettes.component';
 import { DetailsRecetteComponent } from './components/details-recette/details-recette.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-
-  { path: '', component: HomeComponent },
-  { path: 'authentification', component: AuthentificationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: LoginComponent },
   { path: 'gestion-recettes', component: GestionRecettesComponent },
   { path: ':id', component: DetailsRecetteComponent },
-
-
-
-
 ];
 
 @NgModule({
--- front-end-recette-management-qualite-angular/src/app/app.module.ts
+++ front-end-recette-management-qualite-angular/src/app/app.module.ts
@@ -4,22 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GestionRecettesComponent } from './components/gestion-recettes/gestion-recettes.component';
-import { AuthentificationComponent } from './components/authentification/authentification.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsRecetteComponent } from './components/details-recette/details-recette.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GestionRecettesComponent,
-    AuthentificationComponent,
     HomeComponent,
     DetailsRecetteComponent,
     FooterComponent,
     NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
